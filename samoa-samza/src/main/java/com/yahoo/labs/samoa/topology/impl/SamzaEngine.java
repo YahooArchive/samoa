2da995ba76f16fc46f05a499acd6fec57775bc4e
caseyvu <vuanhthu888@gmail.com>
2014-05-06 17:46:14 +0200

    Create kafka streams after SamzaConfigFactory has check and correct all streams' system

--- samoa-samza/src/main/java/com/yahoo/labs/samoa/topology/impl/SamzaEngine.java
+++ samoa-samza/src/main/java/com/yahoo/labs/samoa/topology/impl/SamzaEngine.java
@@ -89,6 +89,17 @@ public class SamzaEngine {
 			return;
 		}
 		
+		// Create kafka streams
+		Set<Stream> streams = topo.getStreams(); 
+		for (Stream stream:streams) {
+			SamzaStream samzaStream = (SamzaStream) stream;
+			List<SamzaSystemStream> systemStreams = samzaStream.getSystemStreams();
+			for (SamzaSystemStream systemStream:systemStreams) {
+				// all streams should be kafka streams
+				SystemsUtils.createKafkaTopic(systemStream.getStream(),systemStream.getParallelism());
+			}
+		}
+		
 		// Submit the jobs with those configs
 		for (MapConfig config:configs) {
 			logger.info("Config:{}",config);
@@ -176,17 +187,6 @@ public class SamzaEngine {
 	public static void submitTopology(SamzaTopology topo) {
 		// Setup SystemsUtils
 		engine._setupSystemsUtils();
-		
-		// Create kafka streams
-		Set<Stream> streams = topo.getStreams(); 
-		for (Stream stream:streams) {
-			SamzaStream samzaStream = (SamzaStream) stream;
-			List<SamzaSystemStream> systemStreams = samzaStream.getSystemStreams();
-			for (SamzaSystemStream systemStream:systemStreams) {
-				// all streams should be kafka streams
-				SystemsUtils.createKafkaTopic(systemStream.getStream(),systemStream.getParallelism());
-			}
-		}
 		
 		// Submit topology
 		engine._submitTopology(topo);
