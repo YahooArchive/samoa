b49d1d1044d6641860821a751b3e96943b0745b0
CasCas <casey_vu@live.com>
2014-06-27 15:29:10 +0200

    FIx BasicClassfificationPerformanceEvaluator: to extend ClassificationPerfEvaluator instead of PerfEvaluator

--- samoa-api/src/main/java/com/yahoo/labs/samoa/evaluation/BasicClassificationPerformanceEvaluator.java
+++ samoa-api/src/main/java/com/yahoo/labs/samoa/evaluation/BasicClassificationPerformanceEvaluator.java
@@ -33,7 +33,7 @@ import com.yahoo.labs.samoa.instances.Utils;
  * @version $Revision: 7 $
  */
 public class BasicClassificationPerformanceEvaluator extends AbstractMOAObject implements
-        PerformanceEvaluator {
+        ClassificationPerformanceEvaluator {
 
     private static final long serialVersionUID = 1L;
 
